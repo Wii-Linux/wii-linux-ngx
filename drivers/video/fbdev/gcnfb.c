347c2ef88ce0608fbbcc08e734e9603561f91232
techflashYT <officialTechflashYT@gmail.com>
2025-05-31 16:20:46 -0700

    video: gcnfb: get it compiling on 5.10
    The old i2c code didn't work anyways, so the parts that don't build here
    can be dropped.

--- drivers/video/fbdev/gcnfb.c
+++ drivers/video/fbdev/gcnfb.c
@@ -1277,7 +1277,7 @@ static void vi_setup_tv_mode(struct vi_ctl *ctl)
 	std = (var->xres_virtual * TV_BYTES_PER_PIXEL) / VI_HORZ_WORD_SIZE;
 	if (!(mode->flags & VI_VMF_PROGRESSIVE))
 		std *= 2;
-	ppl = _ALIGN_UP((var->xoffset & VI_HORZ_ALIGN) + var->xres,
+	ppl = ALIGN((var->xoffset & VI_HORZ_ALIGN) + var->xres,
 			VI_HORZ_ALIGN+1);
 	out_be16(io_base + VI_PCR,
 		 vi_pcr_std(std) |
@@ -1790,7 +1790,7 @@ static int vi_attach_ave(struct vi_ctl *ctl, struct i2c_client *client)
 
 	spin_lock(&ctl->lock);
 	if (!ctl->i2c_client) {
-		ctl->i2c_client = i2c_use_client(client);
+		ctl->i2c_client = client;
 		spin_unlock(&ctl->lock);
 		drv_printk(KERN_INFO, "AVE-RVL support loaded\n");
 		return 0;
@@ -1811,7 +1811,7 @@ static void vi_dettach_ave(struct vi_ctl *ctl)
 		client = ctl->i2c_client;
 		ctl->i2c_client = NULL;
 		spin_unlock(&ctl->lock);
-		i2c_release_client(client);
+		/*i2c_release_client(client);*/
 		drv_printk(KERN_INFO, "AVE-RVL support unloaded\n");
 		return;
 	}
@@ -2009,7 +2009,7 @@ static int vifb_check_var(struct fb_var_screeninfo *var, struct fb_info *info)
 
 	yres = var->yres;
 	if (yres & VI_VERT_ALIGN)
-		yres = _ALIGN_UP(yres, VI_VERT_ALIGN+1);
+		yres = ALIGN(yres, VI_VERT_ALIGN+1);
 	if (yres > mode->height) {
 		if (!nostalgic) {
 			drv_printk(KERN_ERR, "yres %u out of bounds\n", yres);
@@ -2031,7 +2031,7 @@ static int vifb_check_var(struct fb_var_screeninfo *var, struct fb_info *info)
 
 	xres = var->xres;
 	if (xres & VI_HORZ_ALIGN)
-		xres = _ALIGN_UP(xres, VI_HORZ_ALIGN+1);
+		xres = ALIGN(xres, VI_HORZ_ALIGN+1);
 	if (xres > mode->width) {
 		drv_printk(KERN_ERR, "xres %u (%u) out of bounds (max %u)\n", var->xres, xres, mode->width);
 		return -EINVAL;
@@ -2041,7 +2041,7 @@ static int vifb_check_var(struct fb_var_screeninfo *var, struct fb_info *info)
 
 	xres_virtual = var->xres_virtual;
 	if (xres_virtual & VI_HORZ_ALIGN)
-		xres_virtual = _ALIGN_UP(xres_virtual, VI_HORZ_ALIGN+1);
+		xres_virtual = ALIGN(xres_virtual, VI_HORZ_ALIGN+1);
 	if (!xres_virtual || xres_virtual < xres)
 		xres_virtual = xres;
 
